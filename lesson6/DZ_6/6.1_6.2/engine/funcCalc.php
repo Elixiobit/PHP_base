039793b1086b526bf7f9d6d457eec55089504504
Ilya Sushkov <pyh3@yandex.ru>
2020-05-22 12:10:58 +0300

    dz_6_up

--- lesson6/DZ_6/6.1_6.2/engine/funcCalc.php
+++ lesson6/DZ_6/6.1_6.2/engine/funcCalc.php
@@ -1,20 +1,20 @@
 <?php
-function sum($paramOne, $paramTwo)
+function sum(int $paramOne, int $paramTwo): int
 {
     return $paramOne + $paramTwo;
 }
 
-function difference($paramOne, $paramTwo)
+function difference(int $paramOne, int $paramTwo): int
 {
     return $paramOne - $paramTwo;
 }
 
-function multiplication($paramOne, $paramTwo)
+function multiplication(int $paramOne, int $paramTwo): int
 {
     return $paramOne * $paramTwo;
 }
 
-function division($paramOne, $paramTwo)
+function division(int $paramOne, int $paramTwo): float
 {
     if ($paramTwo !== 0) {
         return $paramOne / $paramTwo;
